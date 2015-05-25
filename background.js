0d8d20f99338387df8ae165b07d3a0ae7fc04251
Nick Sandonato <nsandona@us.ibm.com>
2015-05-25 18:48:18 -0400

    Modernize Tab Glutton.

---
+++ background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onStartup.addListener(init);
+chrome.runtime.onInstalled.addListener(init);
+
+chrome.browserAction.onClicked.addListener(popup);
+
+function init() {
+	var count = 0;
+	// set up the initial tab count
+	chrome.windows.getAll({"populate": true}, function(windows) {
+		for( var i in windows ) {
+			count += windows[i].tabs.length;
+		}
+		chrome.browserAction.setBadgeText({text: "" + count});
+	});
+
+	// listen for new tabs
+	chrome.tabs.onCreated.addListener(function(tab) {
+		chrome.browserAction.setBadgeText({text: "" + (++count)});
+	});
+
+	// listen for closed tabs
+	chrome.tabs.onRemoved.addListener(function(tab) {
+		chrome.browserAction.setBadgeText({text: "" + (--count)});
+	});
+}
+
+function popup() {
+	console.log(arguments);
+}
