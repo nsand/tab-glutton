3c58c42f4918de45f0314a9f3680673ac1a0a617
Nick Sandonato <nick.sandonato@gmail.com>
2015-05-28 21:57:05 -0400

    #25: Total tab count can get out of sync from time to time

--- background.js
+++ background.js
@@ -3,35 +3,38 @@ chrome.runtime.onInstalled.addListener(init);*/
 
 
 (function init() {
-	var count = 0;
+
 	// set up the initial tab count
-	chrome.windows.getAll({"populate": true}, function(windows) {
-		for( var i in windows ) {
-			count += windows[i].tabs.length;
-		}
-		chrome.browserAction.setBadgeText({text: "" + count});
+	chrome.windows.getAll({'populate': true}, function (windows) {
+		var count = 0;
+		windows.forEach(function (win) {
+			count += win.tabs.length;
+		});
+		chrome.browserAction.setBadgeText({text: '' + count});
 	});
 
 	// listen for new tabs
-	chrome.tabs.onCreated.addListener(function(tab) {
-		chrome.browserAction.setBadgeText({text: "" + (++count)});
-	});
+	chrome.tabs.onCreated.addListener(updateCount);
 
 	// listen for closed tabs
-	chrome.tabs.onRemoved.addListener(function(tab) {
-		chrome.browserAction.setBadgeText({text: "" + (--count)});
-	});
+	chrome.tabs.onRemoved.addListener(updateCount);
+
+	function updateCount() {
+		chrome.tabs.query({}, function (tabs) {
+			chrome.browserAction.setBadgeText({text: '' + tabs.length});
+		});
+	}
 
 	var popupWindow;
 
-	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-		if (request.action == "popup") {
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.action == 'popup') {
 			if (popupWindow) {
 				popupWindow.close();
 			}
 
-			popupWindow = window.open(chrome.extension.getURL("popup.html"), "Tab Glutton", "width=400,height=530");
-			sendResponse({response: "open"});
+			popupWindow = window.open(chrome.extension.getURL('popup.html'), 'Tab Glutton', 'width=400,height=530');
+			sendResponse({response: 'open'});
 		}
 		else {
 			sendResponse({});
