5f1a5ac748ab2f660b3d7bb7f380d4f2ecb75a1b
Nick Sandonato <nick.sandonato@gmail.com>
2016-06-18 17:36:04 -0400

    Show tabs across all windows

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -6,28 +6,33 @@ export default class Popup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tabs: []
+      windows: []
     };
   }
   componentDidMount() {
     // TODO Do not rely on this to populate the UI; use redux
     chrome.windows.getAll({populate: true}, (windows) => {
-      console.log(windows[0]);
-      this.setState({tabs: windows[0].tabs});
+      this.setState({windows: windows});
     });
   }
   render() {
     return (
-      <section>
-        <header className={styles.heading}>
-          <h2 className={styles.headingText}>{this.state.tabs.length} tabs</h2>
-        </header>
-        <ul className={styles.tabList}>
-          {
-            this.state.tabs.map(tab => <TabItem key={tab.id} tab={tab}></TabItem>)
-          }
-        </ul>
-      </section>
+      <div>
+        {
+          this.state.windows.map($window =>
+            <section>
+              <header className={styles.heading}>
+                <h2 className={styles.headingText}>{$window.tabs.length} tabs</h2>
+              </header>
+              <ul className={styles.tabList}>
+                {
+                  $window.tabs.map(tab => <TabItem key={tab.id} tab={tab}></TabItem>)
+                }
+              </ul>
+            </section>
+          )
+        }
+      </div>
     );
   }
 }
--- src/components/tab-item/tab-item.jsx
+++ src/components/tab-item/tab-item.jsx
@@ -6,9 +6,14 @@ export default class TabItem extends React.Component {
     chrome.tabs.remove(this.props.tab.id);
     console.log(`Closing ${this.props.tab.title}`);
   }
+  focus() {
+    chrome.tabs.update(this.props.tab.id, {active: true}, () => {
+      chrome.windows.update(this.props.tab.windowId, {focused: true})
+    });
+  }
   render() {
     return (
-      <li className={styles.tabItem}>
+      <li className={styles.tabItem} onClick={this.focus.bind(this)}>
         <img src={this.props.tab.favIconUrl || (this.props.tab.favIconUrl === 'chrome://newtab/' ? 'img/chromium_logo.png' : 'img/defaultIcon.png')}></img>
         <span className={styles.tabTitle}>{this.props.tab.title}</span>
         <span className={styles.close} onClick={this.close.bind(this)}>×</span>
