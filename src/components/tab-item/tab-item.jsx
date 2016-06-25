b2af5a9b399a9ae24140e94e7a788c64d26e3692
Nick Sandonato <nick.sandonato@gmail.com>
2016-06-24 22:13:44 -0400

    Conditionally show the url of the tab

--- src/components/tab-item/tab-item.jsx
+++ src/components/tab-item/tab-item.jsx
@@ -3,6 +3,13 @@ import styles from './tab-item.scss';
 import activeStyles from './tab-item-active.scss';
 
 export default class TabItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    this.setState({showUrl: localStorage['showURL']})
+  }
   focus(event) {
     const {tab} = this.props;
     chrome.tabs.update(tab.id, {active: true}, () => {
@@ -11,10 +18,18 @@ export default class TabItem extends React.Component {
   }
   render() {
     const {tab, onClose} = this.props;
+    let additionalDetails;
+    if (this.state.showUrl === 'true') {
+      additionalDetails = <div className={styles.tabText}>{tab.url}</div>;
+    }
+
     return (
       <li className={tab.selected ? activeStyles.tabItem : styles.tabItem} onClick={this.focus.bind(this)}>
         <img src={tab.favIconUrl || (tab.favIconUrl === 'chrome://newtab/' ? 'img/chromium_logo.png' : 'img/defaultIcon.png')}></img>
-        <span className={styles.tabTitle}>{tab.title}</span>
+        <div className={styles.tabDetails}>
+          <div className={styles.tabText}>{tab.title}</div>
+          {additionalDetails}
+        </div>
         <span className={styles.close} onClick={onClose.bind(null, tab)}>×</span>
       </li>
     );
